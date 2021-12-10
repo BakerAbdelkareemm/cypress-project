e991d1bf704f32a9ebf0aaf0570430908d0def10
BakerrAbdelkareemm <bbaakkrrr97@gmail.com>
2021-12-10 18:57:05 +0200

    Make sure cypress is working

---
+++ cypress/integration/baker.feature
@@ -0,0 +1,13 @@
+Feature: baker
+
+    @TC_1
+    Scenario: verify the user can sign up conduit website
+        Given  a user navigated to sign up page
+        And typed in username field a valid username
+        And typed in Email field a valid Email
+        And typed in password field a valid password
+# When  clicks on sign up button
+# Then Start to type your Then step here Start to type your And step here the user should signed up successfully
+
+
+
---
+++ cypress/integration/baker/baker.js
@@ -0,0 +1,39 @@
+import { Given,And,Then,When,     Before,
+    After,} from "cypress-cucumber-preprocessor/steps";
+
+const websiteLink='https://demo.realworld.io/?fbclid=IwAR3DDj55wxw_ip5DwIFQGo4N2s7pQH27vuRfpHkFnd4l_erwueQqiHsrWZI#/register'
+const signUpAPI = "https://api.realworld.io/api/users";
+const userName= "tesdfftttdtCy";
+const email = "tesddtftt@prsedk.com";
+const password = "123455";
+
+
+describe("verify the user can sign up conduit website", ()=>{
+    Given ('a user navigated to sign up page',()=>{
+        cy.visit(websiteLink)
+    
+    })
+    
+    And('typed in username field a valid username',()=>{
+        cy.get('[placeholder=Username]').type(userName) 
+    })
+    
+    And('typed in Email field a valid Email',()=>{
+        cy.get('[placeholder=Email]').type(email);
+    })
+    And('typed in password field a valid password',()=>{
+        cy.get('[placeholder=Password]').type(password)
+    })
+    
+    // When('clicks on sign up button',()=>{
+    //     cy.get('button').contains('Sign up').should('be.visible').click()
+    // })
+    
+    // Then ('the user should signed up successfully',()=>{
+    //         cy.contains('Settings').should('be.visible').click()
+    //     cy.get('button').contains('Or click here to logout.').should('be.visible').click()
+       
+    // })
+})
+
+
